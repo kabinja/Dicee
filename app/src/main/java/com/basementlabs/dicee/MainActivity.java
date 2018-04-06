66234a444914bb13429b7f70ce8cbc2bf142f23b
kabinja <Babychou2008>
2018-04-06 21:48:22 +0200

    add buttons

--- app/src/main/java/com/basementlabs/dicee/MainActivity.java
+++ app/src/main/java/com/basementlabs/dicee/MainActivity.java
@@ -2,12 +2,24 @@ package com.basementlabs.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button rollButton;
+    ImageView leftDice;
+    ImageView rightDice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = (Button)findViewById(R.id.rollButton);
+        leftDice = (ImageView)findViewById(R.id.image_leftdice);
+        rightDice = (ImageView)findViewById(R.id.image_rightdice);
+
+
     }
 }
