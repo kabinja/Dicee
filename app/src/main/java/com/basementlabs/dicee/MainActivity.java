de547d97f01d18d7c7b81e1b6aafbb5a93cb45e5
kabinja <Babychou2008>
2018-06-17 11:18:02 +0200

    add onClickListener on roll button to update dices values

--- app/src/main/java/com/basementlabs/dicee/MainActivity.java
+++ app/src/main/java/com/basementlabs/dicee/MainActivity.java
@@ -3,14 +3,27 @@ package com.basementlabs.dicee;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button rollButton;
     ImageView leftDice;
     ImageView rightDice;
+
+    final int[] diceArray = {
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         leftDice = (ImageView)findViewById(R.id.image_leftdice);
         rightDice = (ImageView)findViewById(R.id.image_rightdice);
 
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
 
+                int right = randomNumberGenerator.nextInt(6);
+                int left = randomNumberGenerator.nextInt(6);
+
+                leftDice.setImageResource(diceArray[right]);
+                rightDice.setImageResource(diceArray[left]);
+            }
+        });
     }
 }
